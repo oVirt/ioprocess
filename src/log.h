23a951d54d32c58f3ef9b398a713d626c46ae469
Saggi Mizrahi <ficoos@gmail.com>
2014-07-27 18:57:03 +0300

    Add missing def-guards from log.h
    Change-Id: I3621889d5f956ce60e29263de87c432bbaf4d5ed
    Signed-off-by: Saggi Mizrahi <ficoos@gmail.com>

--- src/log.h
+++ src/log.h
@@ -1,6 +1,12 @@
+#ifndef __IOPROCESS_LOG_H__
+#define __IOPROCESS_LOG_H__
+
 #include <glib.h>
 
 extern gboolean TRACE_ENABLED;
 
 #define g_trace(...) \
     if (TRACE_ENABLED) { g_debug(__VA_ARGS__); }
+
+#endif
+
