93290cb9151bfb8ff9a382d7d442036c0123d36d
Dima Kuznetsov <dkuznets@redhat.com>
2014-08-02 15:33:25 +0300

    Fix strerror_r usage
    glibc ships with 2 versions of strerror_r
    
    * XSI version that  returns int and does nothing on error
    * GNU version that returns a string but not necessarily in the provided buffer.
    
    This patch introduces gnu_strerror_r that acts like the GNU variant.
    
    Change-Id: I0358b26c8412e0a169483671c8a3ba0936436e7d
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>
    Signed-off-by: Saggi Mizrahi <ficoos@gmail.com>

--- src/utils.c
+++ src/utils.c
@@ -4,21 +4,45 @@
 
 #define MAX_STRERR_SIZE 256
 
+static char *gnu_strerror_r(int error, char *buffer, size_t len) {
+    // Check what strerror_r is present and implement accordingly
+#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+
+    if (strerror_r(error, buffer, len) != 0) {
+        snprintf(buffer, len, "Unknown error %d", error);
+    }
+
+    // XSI version does not promise a terminating zero.
+    if (len) {
+        buffer[len - 1] = '\0';
+    }
+
+    return buffer;
+#else
+    return strerror_r(error, buffer, len);
+#endif
+}
+
+
 const char* iop_strerror(int err) {
     char buff[MAX_STRERR_SIZE];
     int saved_errno = errno;
     char *msg = NULL;
     const char *ret;
 
-    strerror_r(err, buff, MAX_STRERR_SIZE);
-    buff[MAX_STRERR_SIZE - 1] = '\0';
+    ret = gnu_strerror_r(err, buff, MAX_STRERR_SIZE);
     // g_intern_string() only works with utf-8 strings
-    if (!g_get_charset(NULL))
-          msg = g_locale_to_utf8(buff, -1, NULL, NULL, NULL);
+    if (!g_get_charset(NULL)) {
+        msg = g_locale_to_utf8(ret, -1, NULL, NULL, NULL);
+        ret = msg;
+    }
 
     // Put in glib's internal string cache
-    ret = g_intern_string(msg);
-    g_free(msg);
+    ret = g_intern_string(ret);
+
+    if (msg) {
+        g_free(msg);
+    }
 
     errno = saved_errno;
     return ret;
