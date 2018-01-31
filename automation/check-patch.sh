1b41a95356d43eea72a1d4d68ec7002388ffdea9
Nir Soffer <nsoffer@redhat.com>
2018-01-31 22:05:48 +0200

    automation: Build and install artifacts
    I broke build-artifacts too many times this week. Invoke the build
    artifacts script and install the built packages on every build, so we
    can fix build issues before merging.
    
    Change-Id: I90d0900ec7750eb8d36061b755b5ac3b2aae2009
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -xe
 
+# enable complex globs
+shopt -s extglob
+
 automation/setup.sh
 
 # tox on el7 is too old
 pip install tox
 
 make check
+
+automation/build-artifacts.sh
+
+yum install exported-artifacts/!(*.src).rpm
