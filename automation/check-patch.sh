5083191d5fa0af2a7cfc3b5d0d3cad6845d0f669
Sandro Bonazzola <sbonazzo@redhat.com>
2019-10-03 11:01:58 +0200

    automation: add el8 jobs
    Change-Id: I3fca0d3b2b454212d7e67f32681a569657a9f43f
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -6,13 +6,14 @@ shopt -s extglob
 automation/setup.sh
 
 # tox on el7 is too old
-pip install "more-itertools<6.0.0" tox
+${CI_PYTHON} -m pip install "more-itertools<6.0.0" tox
+export PATH="${PATH}:/usr/local/bin"
 
 make check
 
 automation/build-artifacts.sh
 
-if grep -q 'Fedora' /etc/redhat-release; then
+if [ -x /usr/bin/dnf ] ; then
     DNF=dnf
 else
     DNF=yum
