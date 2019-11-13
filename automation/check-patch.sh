0a6211b127fe059c2451a56222c4d62ab4db8cdb
Nir Soffer <nsoffer@redhat.com>
2019-11-13 22:37:45 +0200

    automation: Upgrade pip before installing tox
    Using old pip we can have issues with more-itertools package. Upgrading
    pip fixes this issue.
    
    Change-Id: I56c7be6697dfa533a5bfced67bf6a98da534d57e
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -3,11 +3,18 @@
 # enable complex globs
 shopt -s extglob
 
-automation/setup.sh
+# On Fedora 30 and CentOS 8 pip installs scripts to /usr/local/bin which may
+# not be in PATH.
+export PATH="/usr/local/bin:$PATH"
 
-# tox on el7 is too old
-${CI_PYTHON} -m pip install "more-itertools<6.0.0" tox
-export PATH="${PATH}:/usr/local/bin"
+# First upgrade pip, since older pip versions have issues with installing
+# correct version of requirements.
+${CI_PYTHON} -m pip install --upgrade pip
+
+# Install development requirements.
+${CI_PYTHON} -m pip install --upgrade tox
+
+automation/setup.sh
 
 make check
 
